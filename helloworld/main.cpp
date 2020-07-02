e487b23d3614e3fa00fd3f5a9cbc80207ba38009
Marvling <33942399+Marvling@users.noreply.github.com>
2020-07-01 21:18:55 -0700

    organizing repos

---
+++ helloworld/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std::cout << "s.a gardaş\n";
+    return 0;
+}
---
+++ raylib-sandbox/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <numeric>
+#include <cmath>
+#include <ctime>
+
+#include "raylib.h"
+
+using namespace std;
+
+int globalVar = 0;
+const double decimalVar = 3.142;
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    return 0;
+}
