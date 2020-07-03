b5ebe600eb8c712770b6b251406ae7d339c01bcc
Marvling <33942399+Marvling@users.noreply.github.com>
2020-07-02 21:55:51 -0700

    moving around

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+//short, int, long, long long,
+
+double power(double base, int exponent)
+{
+    double result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        result = base * result;
+    }
+    return result;
+}
+
+void print_number(int number)
+{
+    cout << number << "\n";
+}
+
 int main()
 {
-    std::cout << "s.a gardaş\n";
+
+    int u = 39;
+    char k = '\'';
+    int rakam;
+    int bos = 15;
+
+    string iplik = "helel";
+
+    // cout << "ne kadar?";
+    // cin >> rakam;
+    // cout << "al sana " << rakam << " tane" << std::endl;
+
+    // cout << pow(2, 16) << std::endl;
+    // cout << power(16, 2) << std::endl;
+    // cout << "int u " << (char)u << std::endl;
+    // cout << "char16 k " << (char16_t)k << std::endl;
+
+    // printf("%i\n", bos);
+    // print_number(power(2, 16));
+
+    cout << __FLT_DIG__ << std::endl;
+    cout << __DBL_DIG__ << std::endl;
+
+    cout << iplik << std::endl;
+
     return 0;
 }
