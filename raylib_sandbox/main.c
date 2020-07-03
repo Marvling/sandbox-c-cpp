5c57230c60a6774f6566fd13c81ebdbc9f6197e0
Marvling <33942399+Marvling@users.noreply.github.com>
2020-07-02 23:12:09 -0700

    setup raylib

---
+++ raylib_sandbox/main.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+#include <raylib.h>
+
+//BUILD COMMAND
+// clang -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL  libraylib.a example.c -o my_app3
+
+int main()
+{
+    int yusuf = 12;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib");
+
+    // printf("%s\n", "kenan");
+    // return 0;
+}
